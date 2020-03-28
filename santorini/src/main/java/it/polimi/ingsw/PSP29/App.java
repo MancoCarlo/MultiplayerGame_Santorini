651f1242ac38162d40206e65e0912573a527ae0d
CarloManco <carlo.manco@mail.polimi.it>
2020-03-28 12:12:04 +0100

    Origin

---
+++ santorini/src/main/java/it/polimi/ingsw/PSP29/App.java
@@ -0,0 +1,13 @@
+package it.polimi.ingsw.PSP29;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
