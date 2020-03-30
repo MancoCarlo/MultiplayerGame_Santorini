59e1ec45b395ce4f01edb23fb225691e062d7615
letiziagrassi <61842199+letiziagrassi@users.noreply.github.com>
2020-03-30 22:40:01 +0200

    added JavDoc of Coordinate and Box

---
+++ santorini/src/main/java/it/polimi/ingsw/PSP29/Box_int.java
@@ -0,0 +1,31 @@
+package it.polimi.ingsw.PSP29;
+
+public interface Box_int {
+    /**
+     * check if box is not occupied by a worker
+     * @return true if empty
+     */
+    public boolean isEmpty();
+
+    /**
+     *change state of box from empty to occupied and vice-versa
+     */
+    public void changeState();
+
+    /**
+     * upgrade level of box
+     * @return false if tower is already completed
+     */
+    public boolean upgradeLevel();
+
+    /**
+     * print on monitor the state of box: 0 if empty else 1
+     */
+    public void printEmpty();
+
+    /**
+     * print level of box on monitor
+     */
+    public void printLevel();
+
+}
---
+++ santorini/src/main/java/it/polimi/ingsw/PSP29/Coordinate_int.java
@@ -0,0 +1,12 @@
+package it.polimi.ingsw.PSP29;
+
+public interface Coordinate_int {
+
+    /**
+     * check if two coordinates are adjacent
+     * two adjacent coordinates can't be coincident
+     * @param a coordinate to analyze
+     * @return true when this and a are adjacent
+     */
+    public boolean isNear(Coordinate a);
+}
