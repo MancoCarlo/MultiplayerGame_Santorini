1b5bcecf268abbeb5de0672fd0ce1e691ea6c3cf
CarloManco <carlo.manco@mail.polimi.it>
2020-04-06 20:24:49 +0200

    Fixed bugs

--- santorini/src/main/java/it/polimi/ingsw/PSP29/model/ArtemisTurn.java
+++ santorini/src/main/java/it/polimi/ingsw/PSP29/model/ArtemisTurn.java
@@ -22,35 +22,51 @@ public class ArtemisTurn extends GodTurn{
     @Override
     public boolean move(Match m, Worker w, Coordinate c){
         Scanner scanner = new Scanner(System.in);
-        String x, y;
+        String x, y, answer;
         Coordinate cx = w.getPosition();
         boolean nopower = super.move(m,w,c);
         if(!nopower) return false;
-        if(!super.cantMove(m,w,false)){
-            do{
-                System.out.println("Potere dio attivato!!\nInserisci una nuova coordinata: \t");
-                x=scanner.nextLine();
-                y=scanner.nextLine();
-            }while(Integer.parseInt(x)>m.getRows()-1 || Integer.parseInt(y)>m.getRows()-1 || Integer.parseInt(x)<0 || Integer.parseInt(y)<0 || (Integer.parseInt(x) == cx.getX() && Integer.parseInt(y) == cx.getY()) );
-            Coordinate c1=new Coordinate(Integer.parseInt(x), Integer.parseInt(y));
-            return super.move(m,w,c1) || nopower;
-        }else return true;
+        System.out.println("Vuoi usare il potere di Artemis? 1) SI 2) NO\n");
+        answer = scanner.nextLine();
+        if(answer.equals("1")){
+            if(!super.cantMove(m,w,false)){
+                do{
+                    System.out.println("Potere Artemis attivato!!\nInserisci una nuova coordinata x: \t");
+                    x=scanner.nextLine();
+                    System.out.println("Inserisci una nuova coordinata y: ");
+                    y=scanner.nextLine();
+                }while(Integer.parseInt(x)>m.getRows()-1 || Integer.parseInt(y)>m.getRows()-1 || Integer.parseInt(x)<0 || Integer.parseInt(y)<0 || (Integer.parseInt(x) == cx.getX() && Integer.parseInt(y) == cx.getY()) );
+                Coordinate c1=new Coordinate(Integer.parseInt(x), Integer.parseInt(y));
+                return super.move(m,w,c1) || nopower;
+            }else{
+                System.out.println("Non puoi utilizzare il potere di Artemis");
+            }
+        }
+        return true;
     }
 
     public boolean limited_move(Match m, Worker w, Coordinate c){
         Scanner scanner = new Scanner(System.in);
-        String x, y;
+        String x, y, answer;
         Coordinate cx = w.getPosition();
         boolean nopower = super.limited_move(m,w,c);
         if(!nopower) return false;
-        if(!super.cantMove(m,w,true)){
-            do{
-                System.out.println("Potere dio attivato!!\nInserisci una nuova coordinata: \t");
-                x=scanner.nextLine();
-                y=scanner.nextLine();
-            }while(Integer.parseInt(x)>m.getRows()-1 || Integer.parseInt(y)>m.getRows()-1 || Integer.parseInt(x)<0 || Integer.parseInt(y)<0 || (Integer.parseInt(x) == cx.getX() && Integer.parseInt(y) == cx.getY()));
-            Coordinate c1=new Coordinate(Integer.parseInt(x), Integer.parseInt(y));
-            return super.limited_move(m,w,c1) || nopower;
-        }else return true;
+        System.out.println("Vuoi usare il potere di Artemis? 1) SI 2) NO\n");
+        answer = scanner.nextLine();
+        if(answer.equals("1")){
+            if(!super.cantMove(m,w,false)){
+                do{
+                    System.out.println("Potere Artemis attivato!!\nInserisci una nuova coordinata x: \t");
+                    x=scanner.nextLine();
+                    System.out.println("Inserisci una nuova coordinata y: ");
+                    y=scanner.nextLine();
+                }while(Integer.parseInt(x)>m.getRows()-1 || Integer.parseInt(y)>m.getRows()-1 || Integer.parseInt(x)<0 || Integer.parseInt(y)<0 || (Integer.parseInt(x) == cx.getX() && Integer.parseInt(y) == cx.getY()) );
+                Coordinate c1=new Coordinate(Integer.parseInt(x), Integer.parseInt(y));
+                return super.limited_move(m,w,c1) || nopower;
+            }else{
+                System.out.println("Non puoi utilizzare il potere di Artemis");
+            }
+        }
+        return true;
     }
 }
--- santorini/src/main/java/it/polimi/ingsw/PSP29/model/DemeterTurn.java
+++ santorini/src/main/java/it/polimi/ingsw/PSP29/model/DemeterTurn.java
@@ -15,15 +15,21 @@ public class DemeterTurn extends GodTurn {
     public boolean build(Match m, Worker w, Coordinate c){
         Scanner scanner = new Scanner(System.in);
         Coordinate cx = c;
-        String x,y;
-        boolean nopower = !super.build(m,w,c);
+        String x,y, answer;
+        boolean nopower = super.build(m,w,c);
         if(!nopower) return false;
-        do{
-            System.out.println("Potere dio attivato!!\nInserisci una nuova coordinata: \t");
-            x=scanner.nextLine();
-            y=scanner.nextLine();
-        }while(Integer.parseInt(x)>m.getRows()-1 || Integer.parseInt(y)>m.getRows()-1 || Integer.parseInt(x)<0 || Integer.parseInt(y)<0 || (Integer.parseInt(x) == cx.getX() && Integer.parseInt(y) == cx.getY()));
-        Coordinate c1=new Coordinate(Integer.parseInt(x), Integer.parseInt(y));
+        System.out.println("Vuoi usare il potere di Demeter? 1) SI 2) NO\n");
+        answer = scanner.nextLine();
+        if(answer.equals("1")){
+            do{
+                System.out.println("Potere Demeter attivato!!\nInserisci una nuova coordinata x: \t");
+                x=scanner.nextLine();
+                System.out.println("Inserisci una nuova coordinata y: ");
+                y=scanner.nextLine();
+            }while(Integer.parseInt(x)>m.getRows()-1 || Integer.parseInt(y)>m.getRows()-1 || Integer.parseInt(x)<0 || Integer.parseInt(y)<0 || (Integer.parseInt(x) == cx.getX() && Integer.parseInt(y) == cx.getY()));
+            Coordinate c1=new Coordinate(Integer.parseInt(x), Integer.parseInt(y));
+            return super.build(m,w,c1);
+        }
         return true;
     }
 
