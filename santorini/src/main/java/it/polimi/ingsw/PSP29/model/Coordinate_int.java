29db8be4d8c0298c8bfa931d42fb2fe5a46dc6bd
letiziagrassi <61842199+letiziagrassi@users.noreply.github.com>
2020-04-06 09:49:34 +0200

    updated JavaDocs

--- santorini/src/main/java/it/polimi/ingsw/PSP29/model/Coordinate_int.java
+++ santorini/src/main/java/it/polimi/ingsw/PSP29/model/Coordinate_int.java
@@ -9,4 +9,11 @@ public interface Coordinate_int {
      * @return true when this and a are adjacent
      */
     public boolean isNear(Coordinate a);
+
+    /**
+     * calculate the next coordinate following the direction of movement
+     * @param c coordinate in which I want to move
+     * @return the next coordinate after c or c if there's no following coordinate
+     */
+    public Coordinate nextCoordinate(Coordinate c);
 }
