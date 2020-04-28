4851f475d9cc53a9fa5bd432bb0a540fdd51c371
letiziagrassi <61842199+letiziagrassi@users.noreply.github.com>
2020-04-28 09:41:43 +0200

    fixed errors

--- santorini/src/main/java/it/polimi/ingsw/PSP29/Controller/MinotaurTurn.java
+++ santorini/src/main/java/it/polimi/ingsw/PSP29/Controller/MinotaurTurn.java
@@ -16,7 +16,7 @@ public class MinotaurTurn extends GodTurn {
      */
     @Override
     public boolean winCondition(Match m, Player p) {
-    return true;
+        return super.winCondition(m, p);
     }
 
     /**
@@ -40,7 +40,7 @@ public class MinotaurTurn extends GodTurn {
      */
     @Override
     public boolean move(Match m, Worker w, Coordinate c) {
-        if(w.getPosition().isNear(c) && m.getBoard()[c.getX()][c.getY()].getLevel()!=4){//se la casella è adiacente ma non coincidente e se la torre non è completa
+        if(w.getPosition().isNear(c) && m.getBoard()[c.getX()][c.getY()].getLevel()!=4){
             if (m.getBoard()[c.getX()][c.getY()].isEmpty() && m.getBoard()[c.getX()][c.getY()].level_diff(m.getBoard()[w.getPosition().getX()][w.getPosition().getY()]) <= 1) {
                 m.updateMovement(m.getPlayer(w.getIDplayer()), w.getID(), c);
                 w.changeMoved(true);
@@ -52,8 +52,8 @@ public class MinotaurTurn extends GodTurn {
                 return false;
             }
             else {
-                m.updateMovement(m.getPlayer(m.getBoard()[c.getX()][c.getY()].getWorkerBox().getIDplayer()), m.getBoard()[c.getX()][c.getY()].getWorkerBox().getID(), c1);//costringo il worker a spostarsi
-                m.updateMovement(m.getPlayer(w.getIDplayer()), w.getID(), c); //muovo il mio worker
+                m.updateMovement(m.getPlayer(m.getBoard()[c.getX()][c.getY()].getWorkerBox().getIDplayer()), m.getBoard()[c.getX()][c.getY()].getWorkerBox().getID(), c1);
+                m.updateMovement(m.getPlayer(w.getIDplayer()), w.getID(), c);
                 w.changeMoved(true);
                 return true;
             }
@@ -71,19 +71,19 @@ public class MinotaurTurn extends GodTurn {
      */
     @Override
     public boolean limited_move(Match m, Worker w, Coordinate c) {
-        if(w.getPosition().isNear(c) && m.getBoard()[c.getX()][c.getY()].getLevel()!=4){//se la casella è adiacente ma non coincidente e se la torre non è completa
+        if(w.getPosition().isNear(c) && m.getBoard()[c.getX()][c.getY()].getLevel()!=4){
             if (m.getBoard()[c.getX()][c.getY()].isEmpty() && m.getBoard()[c.getX()][c.getY()].level_diff(m.getBoard()[w.getPosition().getX()][w.getPosition().getY()]) <=0) {
                 m.updateMovement(m.getPlayer(w.getIDplayer()), w.getID(), c);
                 w.changeMoved(true);
                 return true;
             }
-            Coordinate c1 = w.getPosition().nextCoordinate(m, c); //ottengo la coordinata seguente oppure c
+            Coordinate c1 = w.getPosition().nextCoordinate(m, c);
             if(c1.equals(c) || !m.getBoard()[c1.getX()][c1.getY()].isEmpty() || m.getBoard()[c.getX()][c.getY()].isEmpty() || m.getBoard()[c.getX()][c.getY()].getWorkerBox().getIDplayer().equals(w.getIDplayer()) || m.getBoard()[c.getX()][c.getY()].level_diff(m.getBoard()[w.getPosition().getX()][w.getPosition().getY()])>0 || m.getBoard()[c1.getX()][c1.getY()].getLevel()==4)
                 //se la casella c non ha una successiva oppure se la casella successiva non è vuota oppure se la casella indicata non contiene un operaio oppure se l'operaio nella casella è dello stesso giocatore oppure se la casella in cui mi voglio spostare è piu alta della mia casella oppure se la casella successiva è completa
                 return false;
             else {
-                m.updateMovement(m.getPlayer(m.getBoard()[c.getX()][c.getY()].getWorkerBox().getIDplayer()), m.getBoard()[c.getX()][c.getY()].getWorkerBox().getID(), c1);//costringo il worker a spostarsi
-                m.updateMovement(m.getPlayer(w.getIDplayer()), w.getID(), c); //muovo il mio worker
+                m.updateMovement(m.getPlayer(m.getBoard()[c.getX()][c.getY()].getWorkerBox().getIDplayer()), m.getBoard()[c.getX()][c.getY()].getWorkerBox().getID(), c1);
+                m.updateMovement(m.getPlayer(w.getIDplayer()), w.getID(), c);
                 w.changeMoved(true);
                 return true;
             }
@@ -116,10 +116,10 @@ public class MinotaurTurn extends GodTurn {
         } else {
             for (int i = 0; i < m.getRows(); i++) {
                 for (int j = 0; j < m.getColumns(); j++) {
-                    if (w.getPosition().isNear(m.getBoard()[i][j].getLocation()) && m.getBoard()[i][j].getLevel() != 4) { //se la casella è adiacente ma non coincidente e se la torre non è completa
-                        Coordinate c1 = w.getPosition().nextCoordinate(m, m.getBoard()[i][j].getLocation());//restituisce la casella stessa se non ha una successiva
+                    if (w.getPosition().isNear(m.getBoard()[i][j].getLocation()) && m.getBoard()[i][j].getLevel() != 4) {
+                        Coordinate c1 = w.getPosition().nextCoordinate(m, m.getBoard()[i][j].getLocation());
                         if (!m.getBoard()[i][j].getLocation().equals(c1) && m.getBoard()[c1.getX()][c1.getY()].isEmpty() && !m.getBoard()[i][j].isEmpty() && !m.getBoard()[i][j].getWorkerBox().getIDplayer().equals(w.getIDplayer()) && m.getBoard()[w.getPosition().getX()][w.getPosition().getY()].level_diff(m.getBoard()[i][j]) >= -1 && m.getBoard()[c1.getX()][c1.getY()].getLevel() != 4) {
-                            //se la casella c ha una successiva e se la casella successiva  è vuota e se la casella indicata contiene un operaio e se l'operaio nella casella è dello stesso giocatore e se la casella in cui mi voglio spostare non è alta di più di 1 livello e se la casella successiva non è completa
+                            //se la casella c ha una successiva e se la casella successiva  è vuota e se la casella indicata contiene un operaio e se l'operaio nella casella non è dello stesso giocatore e se la casella in cui mi voglio spostare non è alta di più di 1 livello e se la casella successiva non è completa
                             return false;
                         }
                     }
