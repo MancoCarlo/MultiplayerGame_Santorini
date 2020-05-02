217c998bb5a633a7b22c762c4144194402bc35bf
CarloManco <carlo.manco@mail.polimi.it>
2020-05-02 12:06:24 +0200

    added class

---
+++ santorini/src/main/java/it/polimi/ingsw/PSP29/virtualView/UserTimerTask.java
@@ -0,0 +1,30 @@
+package it.polimi.ingsw.PSP29.virtualView;
+
+import java.util.TimerTask;
+
+public class UserTimerTask extends TimerTask {
+    private int seconds=0;
+    private int turnSeconds = 100;
+    private ClientHandler ch;
+    private Server server;
+
+    public UserTimerTask(Server server, ClientHandler ch){
+        this.server = server;
+        this.ch = ch;
+    }
+
+    public void run(){
+        //100 secondi per gestire il turno
+        if(seconds<=turnSeconds){
+            if(seconds % 10 == 0)
+                System.out.println(turnSeconds - seconds+ " seconds remaining");
+            seconds++;
+        }else{
+            server.setTimeout(true);
+            ch.resetConnected();
+            //Disconnessione
+            ch.closeConnection();
+            this.cancel();
+        }
+    }
+}
