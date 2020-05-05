6d981f74be8d8c35cba08d1175deaa5a778849d9
letiziagrassi <61842199+letiziagrassi@users.noreply.github.com>
2020-05-05 10:09:21 +0200

    revised HephaestusTurn

--- santorini/src/main/java/it/polimi/ingsw/PSP29/Controller/HephaestusTurn.java
+++ santorini/src/main/java/it/polimi/ingsw/PSP29/Controller/HephaestusTurn.java
@@ -8,7 +8,6 @@ import it.polimi.ingsw.PSP29.virtualView.ClientHandler;
 import it.polimi.ingsw.PSP29.virtualView.Server;
 
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class HephaestusTurn extends GodTurn {
     public HephaestusTurn(Turn turn) {
@@ -18,25 +17,54 @@ public class HephaestusTurn extends GodTurn {
 
     /**
      * allows w to build two times but it has to be in the same box and not a dome
+     *
      * @param m match played
-     * @param w worker that must build
-     * @param c location of the box where w can build two times
-     * @return true if w has built at least once
+     * @param ch clientHandler that must build
+     * @param server manage the interaction with client
+     * @return true if w has built at least once, else false
      */
-   /* @Override
-    public boolean build(Match m, Worker w, Coordinate c) {
-        boolean nopower = super.build(m,w,c);
-        if(!nopower) return false;
-        Scanner scanner = new Scanner(System.in);
-        String answer;
-        if(m.getBoard()[c.getX()][c.getY()].getLevel()<3) { //non posso costruire una cupola
-            System.out.println("Vuoi usare il potere di Hephaestus? 1) SI 2) NO\n");
-            answer = scanner.nextLine();
-            if (answer.equals("1")) {
-                m.updateBuilding(c);
+
+    @Override
+    public boolean build(Match m, ClientHandler ch, Server server) {
+        int wID = 2;
+        Player p = m.getPlayer(ch.getName());
+        if (p.getWorker(0).getMoved()) wID = 0;
+        if (p.getWorker(1).getMoved()) wID = 1;
+        ArrayList<Coordinate> coordinates = whereCanBuild(m, ch, wID);
+        server.write(ch, "serviceMessage", "Build: ");
+        if (coordinates.size() != 0) {
+            Coordinate c = null;
+            server.write(ch, "serviceMessage", printCoordinates(coordinates));
+            server.write(ch, "interactionServer", "Where you want to build?\n");
+            int id;
+            while (true) {
+                try {
+                    id = Integer.parseInt(server.read(ch));
+                    if (id < 0 || id >= coordinates.size()) {
+                        server.write(ch, "serviceMessage", "Invalid input\n");
+                        server.write(ch, "interactionServer", "Try another index: ");
+                        continue;
+                    }
+                    break;
+                } catch (NumberFormatException e) {
+                    server.write(ch, "serviceMessage", "Invalid input\n");
+                    server.write(ch, "interactionServer", "Try another index: ");
+                }
             }
+            c = coordinates.get(id);
+            m.updateBuilding(c);
+            m.getBoard()[c.getX()][c.getY()].setLevelledUp(true);
+            p.getWorker(wID).changeBuilt(true);
+            String power;
+            if (m.getBoard()[c.getX()][c.getY()].getLevel() < 3) {
+                server.write(ch, "interactionServer", "Would you like to build another block on your previous one?\n1) Yes\n2) No\n");
+                power = server.read(ch);
+                if (power.equals("1"))
+                    m.updateBuilding(c);
+            }
+            return true;
+        } else {
+            return false;
         }
-        return true;
     }
-*/
 }
