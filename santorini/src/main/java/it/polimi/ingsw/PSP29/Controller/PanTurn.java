0955b3316df2d7da2c7993f749e4f2a5c1307cee
letiziagrassi <61842199+letiziagrassi@users.noreply.github.com>
2020-05-12 21:09:45 +0200

    modified PanTurn

--- santorini/src/main/java/it/polimi/ingsw/PSP29/Controller/PanTurn.java
+++ santorini/src/main/java/it/polimi/ingsw/PSP29/Controller/PanTurn.java
@@ -1,10 +1,6 @@
 package it.polimi.ingsw.PSP29.Controller;
 
 import it.polimi.ingsw.PSP29.model.*;
-import it.polimi.ingsw.PSP29.virtualView.ClientHandler;
-import it.polimi.ingsw.PSP29.virtualView.Server;
-
-import java.util.ArrayList;
 
 public class PanTurn extends GodTurn {
 
@@ -37,38 +33,4 @@ public class PanTurn extends GodTurn {
         return false;
     }
 
-
-    @Override
-    public boolean build(Match m, ClientHandler ch, Server server) {
-        return super.build(m, ch, server);
-    }
-
-    @Override
-    public boolean move(Match m, ClientHandler ch, Server server, boolean athenaOn) {
-        return super.move(m, ch, server, athenaOn);
-    }
-
-    @Override
-    public boolean canBuildIn(Match match,Worker w,Coordinate c){
-        return super.canBuildIn(match, w, c);
-    }
-
-    @Override
-    public boolean canMoveTo(Match m,Worker w,Coordinate c, boolean athena){ return super.canMoveTo(m,w,c,athena);
-    }
-
-    @Override
-    public ArrayList<Coordinate> whereCanMove(Match match, ClientHandler ch, int id, boolean athenaOn) {
-        return super.whereCanMove(match,ch,id,athenaOn);
-    }
-
-    @Override
-    public ArrayList<Coordinate> whereCanBuild(Match match, ClientHandler ch, int id) {
-        return super.whereCanBuild(match, ch, id);
-    }
-
-    @Override
-    public String printCoordinates(ArrayList<Coordinate> coordinates) {
-        return super.printCoordinates(coordinates);
-    }
 }
