2fe82597d802d1bffb227b4b3b5967af478166fe
letiziagrassi <61842199+letiziagrassi@users.noreply.github.com>
2020-05-12 09:12:13 +0200

    revised HephaestusTurn

--- santorini/src/main/java/it/polimi/ingsw/PSP29/Controller/HephaestusTurn.java
+++ santorini/src/main/java/it/polimi/ingsw/PSP29/Controller/HephaestusTurn.java
@@ -1,9 +1,6 @@
 package it.polimi.ingsw.PSP29.Controller;
 
-import it.polimi.ingsw.PSP29.model.Coordinate;
-import it.polimi.ingsw.PSP29.model.Match;
-import it.polimi.ingsw.PSP29.model.Player;
-import it.polimi.ingsw.PSP29.model.Worker;
+import it.polimi.ingsw.PSP29.model.*;
 import it.polimi.ingsw.PSP29.virtualView.ClientHandler;
 import it.polimi.ingsw.PSP29.virtualView.Server;
 
@@ -32,11 +29,11 @@ public class HephaestusTurn extends GodTurn {
         if (p.getWorker(0).getMoved()) wID = 0;
         if (p.getWorker(1).getMoved()) wID = 1;
         ArrayList<Coordinate> coordinates = whereCanBuild(m, ch, wID);
-        server.write(ch, "serviceMessage", "Build: ");
+        server.write(ch, "serviceMessage", "MSGE-Build: ");
         if (coordinates.size() != 0) {
             Coordinate c = null;
-            server.write(ch, "serviceMessage", printCoordinates(coordinates));
-            server.write(ch, "interactionServer", "Where you want to build?\n");
+            server.write(ch, "serviceMessage", "LIST-"+printCoordinates(coordinates));
+            server.write(ch, "interactionServer", "TURN-Where you want to build?\n");
             int id;
             while (true) {
                 try {
@@ -49,14 +46,14 @@ public class HephaestusTurn extends GodTurn {
                         id = Integer.parseInt(msg);
                     }
                     if (id < 0 || id >= coordinates.size()) {
-                        server.write(ch, "serviceMessage", "Invalid input\n");
-                        server.write(ch, "interactionServer", "Try another index: ");
+                        server.write(ch, "serviceMessage", "MSGE-Invalid input\n");
+                        server.write(ch, "interactionServer", "INDX-Try another index: ");
                         continue;
                     }
                     break;
                 } catch (NumberFormatException e) {
-                    server.write(ch, "serviceMessage", "Invalid input\n");
-                    server.write(ch, "interactionServer", "Try another index: ");
+                    server.write(ch, "serviceMessage", "MSGE-Invalid input\n");
+                    server.write(ch, "interactionServer", "INDX-Try another index: ");
                 }
             }
             c = coordinates.get(id);
@@ -66,7 +63,7 @@ public class HephaestusTurn extends GodTurn {
             String power;
             if (m.getBoard()[c.getX()][c.getY()].getLevel() < 3) {
                 server.write(ch, "serviceMessage", "LIST-1) YES\n2)NO\n");
-                server.write(ch, "interactionServer", "Would you like to build another block on your previous one?\n1) Yes\n2) No\n");
+                server.write(ch, "interactionServer", "INDX2-Would you like to build another block on your previous one?\n1) Yes\n2) No\n");
                 power = server.read(ch);
                 if (power.equals("1"))
                     m.updateBuilding(c);
