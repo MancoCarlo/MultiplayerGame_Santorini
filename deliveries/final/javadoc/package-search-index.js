c91b869634d9ad9568bb0daafff7b93657a23ab4
CarloManco <carlo.manco@mail.polimi.it>
2020-07-03 15:34:11 +0200

    added javadoc

---
+++ deliveries/final/javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"it.polimi.ingsw.PSP29.Controller"},{"l":"it.polimi.ingsw.PSP29.Controller.Turn"},{"l":"it.polimi.ingsw.PSP29.model"},{"l":"it.polimi.ingsw.PSP29.view"},{"l":"it.polimi.ingsw.PSP29.view.GUI"},{"l":"it.polimi.ingsw.PSP29.virtualView"}]
